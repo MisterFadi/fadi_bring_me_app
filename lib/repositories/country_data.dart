d056aaede8d24657072035a3dded863e402ccbfb
Fadi Bdiwi <fadibdiwi98@yahoo.de>
2024-10-21 17:43:58 +0200

    add Country Data

---
+++ lib/repositories/country_data.dart
@@ -0,0 +1,40 @@
+import 'package:fadi_bring_me_app/models/country_group.dart';
+
+List<CountryGroup> countrys = [
+  CountryGroup(
+    imagePath: "assets/images/Flag_of_Germany.jpg",
+    imageTitle: "Germany",
+  ),
+  CountryGroup(
+    imagePath: "assets/images/Flag_of_GB.jpg",
+    imageTitle: "English",
+  ),
+  CountryGroup(
+    imagePath: "assets/images/Flag_of_France.jpg",
+    imageTitle: "France",
+  ),
+  CountryGroup(
+    imagePath: "assets/images/Flag_of_Belgium.jpg",
+    imageTitle: "Belgium",
+  ),
+  CountryGroup(
+    imagePath: "assets/images/Flag_of_Syria.jpg",
+    imageTitle: "Arabisch",
+  ),
+  CountryGroup(
+    imagePath: "assets/images/Flag_of_Denmark.jpg",
+    imageTitle: "Denmark",
+  ),
+  CountryGroup(
+    imagePath: "assets/images/Flag_of_Syria.jpg",
+    imageTitle: "Arabisch",
+  ),
+  CountryGroup(
+    imagePath: "assets/images/Flag_of_Finland.jpg",
+    imageTitle: "Finland",
+  ),
+  CountryGroup(
+    imagePath: "assets/images/Flag_of_Syria.jpg",
+    imageTitle: "Arabisch",
+  ),
+];
