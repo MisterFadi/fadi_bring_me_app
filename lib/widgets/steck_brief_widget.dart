78e634658abd0da6c5d1cf8789633c25025e4ff8
Fadi Bdiwi <fadibdiwi98@yahoo.de>
2024-10-21 17:45:02 +0200

    add SteckBriefWidget

---
+++ lib/widgets/steck_brief_widget.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class SteckBriefWidget extends StatelessWidget {
+  const SteckBriefWidget({
+    super.key,
+    required this.image,
+    required this.title,
+  });
+
+  final String image;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        //mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const SizedBox(width: 70),
+          Container(height: 70, width: 70, child: Image.asset(image)),
+          const SizedBox(
+            width: 40,
+          ),
+          Text(
+            title,
+            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+          ),
+          const SizedBox(height: 10),
+        ],
+      ),
+    );
+  }
+}
