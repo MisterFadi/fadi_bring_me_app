9263d7f53ca64fda81a8cf9513b39789be3ec6a6
Fadi Bdiwi <fadibdiwi98@yahoo.de>
2024-10-27 16:03:05 +0100

    add new themes

--- lib/config/themes.dart
+++ lib/config/themes.dart
@@ -2,12 +2,16 @@ import 'package:flutter/material.dart';
 
 ThemeData myTheme = ThemeData(
     textTheme: const TextTheme(
+        headlineLarge: TextStyle(
+            fontSize: 30, color: Colors.white, fontWeight: FontWeight.normal),
         headlineMedium: TextStyle(
             fontSize: 25, color: Colors.black, fontWeight: FontWeight.bold),
+        headlineSmall: TextStyle(
+            fontSize: 18, color: Colors.blue, fontWeight: FontWeight.normal),
         bodyLarge: TextStyle(
-            fontSize: 18, color: Colors.black, fontWeight: FontWeight.bold),
+            fontSize: 18, color: Colors.white, fontWeight: FontWeight.bold),
         bodyMedium: TextStyle(fontSize: 16, color: Colors.black),
-        bodySmall: TextStyle(fontSize: 14, color: Colors.black),
+        bodySmall: TextStyle(fontSize: 14, color: Colors.white),
         displaySmall: TextStyle(
             fontSize: 10, color: Colors.black, fontWeight: FontWeight.normal)),
     appBarTheme: const AppBarTheme(
