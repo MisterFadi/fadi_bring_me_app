9a7c0280cb02d9ad69cdd19aa0835a684670188c
Fadi Bdiwi <fadibdiwi98@yahoo.de>
2024-11-22 16:26:20 +0100

    add mockDatabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,28 @@
 import 'package:fadi_bring_me_app/config/themes.dart';
+import 'package:fadi_bring_me_app/database/mock_database.dart';
+import 'package:fadi_bring_me_app/database/repository/database_repository.dart';
 import 'package:fadi_bring_me_app/features/home_screen/screen/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  final repository = MockDatabase();
+
+  runApp(MainApp(repository: repository));
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({
-    super.key,
-  });
+  MainApp({super.key, required DatabaseRepository repository});
+
+  final DatabaseRepository repository = MockDatabase();
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: myTheme,
-      home: const Scaffold(
+      home: Scaffold(
         backgroundColor: Colors.green,
-        body: HomeScreen(),
+        body: HomeScreen(repository: repository),
       ),
     );
   }
