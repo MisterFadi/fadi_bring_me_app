349c1785df2cfc082dd28af659a20ea89125273c
Fadi Bdiwi <fadibdiwi98@yahoo.de>
2024-12-20 14:26:44 +0100

    SharedPreferencesAuth implements AuthRepo

---
+++ lib/database/mock/shared_preferences_auth.dart
@@ -0,0 +1,37 @@
+import 'package:fadi_bring_me_app/database/repository/auth_repo.dart';
+import 'package:fadi_bring_me_app/shared/user.dart';
+
+class SharedPreferencesAuth implements AuthRepo {
+  @override
+  // TODO: implement authStateChanges
+  Stream<AppUser?> get authStateChanges => throw UnimplementedError();
+
+  @override
+  Future<AppUser?> createUserWithEmailAndPassword(
+      String email, String password) {
+    // TODO: implement createUserWithEmailAndPassword
+    throw UnimplementedError();
+  }
+
+  @override
+  // TODO: implement currentUser
+  AppUser? get currentUser => throw UnimplementedError();
+
+  @override
+  Future<AppUser?> signInWithEmailAndPassword(String email, String password) {
+    // TODO: implement signInWithEmailAndPassword
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<AppUser?> signInWithGoogle() {
+    // TODO: implement signInWithGoogle
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> signOut() {
+    // TODO: implement signOut
+    throw UnimplementedError();
+  }
+}
