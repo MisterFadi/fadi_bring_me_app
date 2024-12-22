f0ba0c60ae7f32eba7b2b08d8a2bfc75e4e5ac4c
Fadi Bdiwi <fadibdiwi98@yahoo.de>
2024-12-22 22:19:07 +0100

    add Streambuilder

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,9 @@ import 'package:fadi_bring_me_app/database/firebase/firebase_auth_repo.dart';
 import 'package:fadi_bring_me_app/database/firebase/firebase_database_repo.dart';
 import 'package:fadi_bring_me_app/database/repository/auth_repo.dart';
 import 'package:fadi_bring_me_app/database/repository/database_repository.dart';
+import 'package:fadi_bring_me_app/features/authentification/screens/login_screen.dart';
 import 'package:fadi_bring_me_app/features/home_screen/screen/home_screen.dart';
 import 'package:fadi_bring_me_app/firebase_options.dart';
-import 'package:fadi_bring_me_app/splash_screen/splash_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -34,20 +34,33 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final authRepo = Provider.of<AuthRepo>(context, listen: false);
     return MaterialApp(
       theme: myTheme,
       debugShowCheckedModeBanner: false,
-      initialRoute: "/",
-      routes: {
-        "/": (context) => const SplashScreen(),
-        "/home": (context) => const Scaffold(
-              body: HomeScreen(),
-            ),
-      },
-      // home: Scaffold(
-      //   backgroundColor: Colors.green,
-      //   body: HomeScreen(repository: repository),
-      // ),
+      // initialRoute: "/",
+      // routes: {
+      //   "/": (context) => const SplashScreen(),
+      //   "/home": (context) => const Scaffold(
+      //         body: HomeScreen(),
+      //       ),
+      // },
+      home: StreamBuilder(
+        stream: authRepo.authStateChanges,
+        builder: (context, snapshot) {
+          final user = snapshot.data;
+          if (snapshot.connectionState == ConnectionState.active) {
+            if (user == null) {
+              return const HomeScreen();
+            } else {
+              return const LoginScreen(
+                language: '',
+              );
+            }
+          }
+          return const Center(child: CircularProgressIndicator());
+        },
+      ),
     );
   }
 }
