0a887dec38e9bb61d6e231b750401ebbaad118d0
Fadi Bdiwi <fadibdiwi98@yahoo.de>
2025-02-13 14:26:12 +0100

    add SignInButton

---
+++ lib/shared/sign_in_button.dart
@@ -0,0 +1,76 @@
+import 'dart:developer' as dev;
+
+import 'package:fadi_bring_me_app/config/colors.dart';
+import 'package:fadi_bring_me_app/features/authentification/screens/sign_in_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class SignInButton extends StatefulWidget {
+  const SignInButton({
+    super.key,
+    required this.contHeight,
+    required this.contWidth,
+    required this.emailController,
+    required this.passwordController,
+    required String text,
+    required this.onPressed,
+  });
+
+  final double contHeight;
+  final double contWidth;
+  final TextEditingController emailController;
+  final TextEditingController passwordController;
+
+  final Function() onPressed;
+
+  @override
+  State<SignInButton> createState() => _SignInButtonState();
+}
+
+class _SignInButtonState extends State<SignInButton> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  Future<void> login() async {
+    final authRepo = Provider.of<AuthRepository>(context, listen: false);
+    try {
+      await authRepo.signInWithEmailAndPassword(
+        emailController.text.trim(),
+        passwordController.text.trim(),
+      );
+      Navigator.pushNamed(context, "/bottomnavbarwidget");
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Login fehlgeschlagen: ${e.toString()}")),
+      );
+      dev.log("Fehler beim Login: $e");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: widget.onPressed,
+      child: Container(
+        height: widget.contHeight,
+        width: widget.contWidth,
+        decoration: BoxDecoration(
+          border: Border.all(width: 0.5),
+          boxShadow: [
+            BoxShadow(
+              color: appColorLogo,
+            ),
+          ],
+          borderRadius: BorderRadius.circular(9),
+        ),
+        child: const Center(
+          child: Text("Registrieren",
+              style: TextStyle(
+                  color: Colors.green,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold)),
+        ),
+      ),
+    );
+  }
+}
