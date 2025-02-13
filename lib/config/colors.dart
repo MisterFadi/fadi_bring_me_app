d0709479e1a3722e77c872da497909958f38274b
Fadi Bdiwi <fadibdiwi98@yahoo.de>
2025-02-13 14:23:38 +0100

    add const Color

---
+++ lib/config/colors.dart
@@ -0,0 +1,5 @@
+// hier kommt die Funktion damit die App nur vertical genutzt wird
+import 'package:flutter/material.dart';
+
+Color appColor = Colors.green;
+Color appColorLogo = const Color.fromARGB(255, 242, 232, 207);
