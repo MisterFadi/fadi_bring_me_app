dbbd3a53a86894f59d420ddd2e393332314e182e
Fadi Bdiwi <fadibdiwi98@yahoo.de>
2025-02-13 11:27:54 +0100

    change name from ListScreen to ProductScreen

--- lib/shared/bottom_nav_bar_widget.dart
+++ lib/shared/bottom_nav_bar_widget.dart
@@ -1,7 +1,7 @@
 import 'package:fadi_bring_me_app/features/card_screen/screen/card_screen.dart';
 import 'package:fadi_bring_me_app/features/folder_screen/screen/folder_screen.dart';
 import 'package:fadi_bring_me_app/features/kamera_screen/screen/kamera_screen.dart';
-import 'package:fadi_bring_me_app/features/list_screen/screen/list_screen.dart';
+import 'package:fadi_bring_me_app/features/list_screen/screen/product_screen.dart';
 import 'package:fadi_bring_me_app/features/profil_screen/screen/profil_sreen.dart';
 import 'package:flutter/material.dart';
 
@@ -20,7 +20,7 @@ class _BottomNavBarWidgetState extends State<BottomNavBarWidget> {
   Widget build(BuildContext context) {
     // Jetzt übergeben wir repository an FolderScreen
     final List<Widget> widgets = [
-      const ListScreen(),
+      const ProductScreen(),
       const FolderScreen(), // Hier repository übergeben
       const KameraScreen(),
       const CardScreen(),
@@ -28,7 +28,6 @@ class _BottomNavBarWidgetState extends State<BottomNavBarWidget> {
     ];
     return Scaffold(
       bottomNavigationBar: NavigationBar(
-        indicatorColor: Colors.green,
         selectedIndex: currentIndex,
         onDestinationSelected: (index) {
           setState(() {
@@ -39,63 +38,64 @@ class _BottomNavBarWidgetState extends State<BottomNavBarWidget> {
           NavigationDestination(
               selectedIcon: Icon(
                 Icons.list,
-                size: 45,
+                size: 40,
                 color: Colors.white,
               ),
               icon: Icon(
                 Icons.list,
                 color: Colors.black54,
-                size: 45,
+                size: 40,
               ),
               label: ""),
           NavigationDestination(
               selectedIcon: Icon(
                 Icons.folder_open,
-                size: 45,
+                size: 40,
                 color: Colors.white,
               ),
               icon: Icon(
                 Icons.folder_open,
-                size: 45,
+                size: 40,
                 color: Colors.black54,
               ),
               label: ""),
           NavigationDestination(
               selectedIcon: Icon(
                 Icons.add_circle_outline,
-                size: 85,
+                size: 70,
                 color: Colors.white,
               ),
               icon: Icon(
                 Icons.add_circle_outline,
                 color: Colors.black54,
-                size: 85,
+                size: 70,
               ),
               label: ""),
           NavigationDestination(
               selectedIcon: Icon(
                 Icons.shopping_cart_outlined,
-                size: 45,
+                size: 40,
                 color: Colors.white,
               ),
               icon: Icon(
                 Icons.shopping_cart_outlined,
                 color: Colors.black54,
-                size: 45,
+                size: 40,
               ),
               label: ""),
           NavigationDestination(
-              selectedIcon: Icon(
-                Icons.person_2_outlined,
-                size: 45,
-                color: Colors.white,
-              ),
-              icon: Icon(
-                Icons.person_2_outlined,
-                color: Colors.black54,
-                size: 45,
-              ),
-              label: ""),
+            selectedIcon: Icon(
+              Icons.person_2_outlined,
+              size: 40,
+              color: Colors.white,
+            ),
+            icon: Icon(
+              Icons.person_2_outlined,
+              color: Colors.black54,
+              size: 40,
+            ),
+            label: "",
+          ),
         ],
       ),
       body: widgets[currentIndex],
