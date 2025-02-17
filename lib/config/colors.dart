32adb7ba01984dbab8c8b78cf229c5406c530a51
Fadi Bdiwi <fadibdiwi98@yahoo.de>
2025-02-17 22:44:46 +0100

    add App Style Color

--- lib/config/colors.dart
+++ lib/config/colors.dart
@@ -3,3 +3,8 @@ import 'package:flutter/material.dart';
 
 Color appColor = Colors.green;
 Color appColorLogo = const Color.fromARGB(255, 242, 232, 207);
+Color colorTextFormField = Colors.black54;
+Color colorTextFormFieldHintText = Colors.black54;
+Color backgroundColor = const Color.fromARGB(255, 242, 232, 207);
+Color appBarColor = Colors.red;
+Color navBarColor = Colors.orange;
