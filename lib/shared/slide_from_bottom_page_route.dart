ca8318078b5180d5bc0dd9083f0be545d761a234
Fadi Bdiwi <fadibdiwi98@yahoo.de>
2025-02-22 12:34:33 +0100

    curve change to ease

--- lib/shared/slide_from_bottom_page_route.dart
+++ lib/shared/slide_from_bottom_page_route.dart
@@ -8,7 +8,7 @@ class SlideFromBottomPageRoute<T> extends PageRouteBuilder<T> {
           transitionsBuilder: (context, animation, secondaryAnimation, child) {
             const begin = Offset(0.0, 1.0);
             const end = Offset.zero;
-            const curve = Curves.easeInOut;
+            const curve = Curves.ease;
             var tween =
                 Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
             var offsetAnimation = animation.drive(tween);
