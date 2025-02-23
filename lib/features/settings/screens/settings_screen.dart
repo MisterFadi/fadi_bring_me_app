aa0a5ba36e3a2a5238a3de2f93a8ec98bfef3f66
Fadi Bdiwi <fadibdiwi98@yahoo.de>
2025-02-23 23:26:52 +0100

    remove underlined

--- lib/features/settings/screens/settings_screen.dart
+++ lib/features/settings/screens/settings_screen.dart
@@ -9,10 +9,10 @@ class SettingsScreen extends StatefulWidget {
   });
 
   @override
-  _SettingsScreenState createState() => _SettingsScreenState();
+  SettingsScreenState createState() => SettingsScreenState();
 }
 
-class _SettingsScreenState extends State<SettingsScreen> {
+class SettingsScreenState extends State<SettingsScreen> {
   String? username;
 
   @override
