71bfd3ae28c62f0c85a524410db77f763ee42d09
Fadi Bdiwi <fadibdiwi98@yahoo.de>
2025-02-24 02:27:04 +0100

    extra intro slider

---
+++ lib/shared/intro_slider.dart
@@ -0,0 +1,28 @@
+/*
+import 'package:flutter/widgets.dart';
+
+class IntroSliderPages extends StatefulWidget {
+  const IntroSliderPages({super.key});
+
+  @override
+  State<IntroSliderPages> createState() => _IntroSliderPagesState();
+}
+
+class _IntroSliderPagesState extends State<IntroSliderPages> {
+  List<Slide> slides = List();
+
+  @override
+  void initState() {
+    super.initState();
+    slides.add(Slide(title: "afsdf", description: "dfsdf"));
+  }
+
+  List<Widget> renderListCustomTabs() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+*/
